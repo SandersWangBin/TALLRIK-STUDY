10722bfcb19a9541492388359c46e2c476286bb9
SandersWangBin <sanderswangbin@gmail.com>
2017-08-24 16:10:36 +0200

    tallrik: support class obj and fire

--- src/Events.py
+++ src/Events.py
@@ -5,42 +5,62 @@ import os, imp
 class Events(object):
     SYMBOL_COLON = ':'
     SYMBOL_DOT   = '.'
+    SYMBOL_SIGN  = '#'
 
-    TYPE_MOD   = 'MOD'
-    TYPE_FUNC  = 'FUNC'
-    TYPE_CLASS = 'CLASS'
+    TYPE_MOD  = 'MOD'
+    TYPE_FUNC = 'FUNC'
+    TYPE_OBJ  = 'OBJ'
 
-    KEYWORD_TYPE  = '$TYPE'
-    KEYWORD_MOD   = '$MOD'
-    KEYWORD_CLASS = '$CLASS'
-    KEYWORD_FUNC  = '$FUNC'
+    KEYWORD_TYPE     = '$TYPE'
+    KEYWORD_MOD      = '$MOD'
+    KEYWORD_OBJ      = '$OBJ'
+    KEYWORD_OBJFUNC  = '$OBJFUNC'
+    KEYWORD_FUNC     = '$FUNC'
     KEYWORD_INSTANCE = '$INSTANCE'
 
     def __init__(self):
         self.events = dict()
 
-    def register(self, eventStr):
+    def register(self, eventStr, *args):
         r, t, a = self._parserEventStr(eventStr)
         if r:
             me = self._touchModElement(a[0])
             if t == Events.TYPE_FUNC:
                 self._touchFuncElement(me, a[1])
-            elif t == Events.TYPE_CLASS:
-                oe = self._touchClassElement(me, a[1])
+            elif t == Events.KEYWORD_OBJ:
+                self._touchObjElement(me, a[1], *args)
+            elif t == Events.KEYWORD_OBJFUNC:
+                oe = self._touchObjElement(me, a[1], *args)
                 self._touchFuncElement(oe, a[2])
+
+    def fire(self, eventStr, *args):
+        r, t, a = self._parserEventStr(eventStr)
+        if r:
+            me = self._getModElement(a[0])
+            if t == Events.TYPE_FUNC and me != None:
+                self._getFuncElement(me, a[1])[Events.KEYWORD_INSTANCE](*args)
+            elif t == Events.KEYWORD_OBJFUNC and me != None:
+                oe = self._getObjElement(me, a[1])
+                if oe != None:
+                    self._getFuncElement(oe, a[2])[Events.KEYWORD_INSTANCE](*args)
 
     def _parserEventStr(self, eventStr):
         if Events.SYMBOL_COLON in eventStr:
             m, f = eventStr.rsplit(Events.SYMBOL_COLON, 1)
             if Events.SYMBOL_DOT in f:
                 o, f = f.rsplit(Events.SYMBOL_DOT, 1)
-                return True, Events.TYPE_CLASS, (m,o,f)
+                return True, Events.KEYWORD_OBJFUNC, (m,o,f)
+            elif Events.SYMBOL_SIGN in f:
+                return True, Events.KEYWORD_OBJ, (m, f)
             else:
                 return True, Events.TYPE_FUNC, (m, f)
         return False, None, None
 
+    def _getModElement(self, m):
+        return self.events.get(m, None)
+
     def _touchModElement(self, m):
-        me = self.events.get(m, None)
+        me = self._getModElement(m)
         if me == None:
             self.events[m] = dict()
             self.events[m][Events.KEYWORD_TYPE] = Events.TYPE_MOD
@@ -54,17 +74,28 @@ class Events(object):
         with open(path, 'rb') as fin:
             return imp.load_source(base, path, fin)
 
-    def _touchClassElement(self, e, o):
-        oe = e.get(o, None)
+    def _getObjElement(self, e, o):
+        return e.get(o, None)
+
+    def _getClassName(self, o):
+        return o.rsplit(Events.SYMBOL_SIGN, 1)[0]
+
+    def _touchObjElement(self, e, o, *args):
+        oe = self._getObjElement(e, o)
         if oe == None:
             e[o] = dict()
-            e[o][Events.KEYWORD_TYPE] = Events.TYPE_CLASS
-            e[o][Events.KEYWORD_INSTANCE] = getattr(e[Events.KEYWORD_INSTANCE], o)()
+            e[o][Events.KEYWORD_TYPE] = Events.TYPE_OBJ
+            e[o][Events.KEYWORD_INSTANCE] = \
+                getattr(e[Events.KEYWORD_INSTANCE], \
+                self._getClassName(o))(*args)
             return e[o]
         else: return oe
 
+    def _getFuncElement(self, e, f):
+        return e.get(f, None)
+
     def _touchFuncElement(self, e, f):
-        fe = e.get(f, None)
+        fe = self._getFuncElement(e, f)
         if fe == None:
             e[f] = dict()
             e[f][Events.KEYWORD_TYPE] = Events.TYPE_FUNC
@@ -73,4 +104,5 @@ class Events(object):
         else: return fe
 
     def __str__(self):
-        return str(self.events)
+        return 'self.events: ' + str(self.events)
+    
