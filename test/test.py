a331d66962546aa71b9fda95eb367acdbc9e7c66
SandersWangBin <sanderswangbin@gmail.com>
2017-08-26 21:53:17 +0200

    tallrik: update test

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import sys
 
 sys.path.append('../src/')
@@ -5,9 +7,18 @@ from Events import Events
 
 e = Events()
 e.register('../userDesign/hello.py:printHello')
+e.register('../userDesign/hello.py:Hello#0', 456)
 e.register('../userDesign/hello.py:Hello#0.printText')
 
 print e
 
 e.fire('../userDesign/hello.py:printHello')
-e.fire('../userDesign/hello.py:Hello#0.printText', 'ok')
+e.fire('../userDesign/hello.py:Hello#0.printText', 123, 'ok')
+
+
+from Parser import *
+root = genBinTree(genRpnList('<a>; ( (<b>,<c>,<d>*[0:]) | <e> ; <f>*[5] )'))
+print
+printTreeUpDown(root)
+print
+printTreeDownUp(root)
